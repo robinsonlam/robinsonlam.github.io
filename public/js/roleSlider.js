3553efd08d79f9dc074e9719913003240c0978b6
Robinson Lam <robinsonlam94@gmail.com>
2019-02-08 02:32:07 +1100

    feat(threejs): install threejs, add script for roleSlider, add example cube to homepage

---
+++ public/js/roleSlider.js
@@ -0,0 +1,12 @@
+const roleSliderData = require(['/roleSlider.json']);
+
+const initiateRoleSlider = () => {
+
+	console.log('roleSliderData');
+
+	let category = $(".webRole .category").text();
+	let role = $(".webRole .role").text()
+
+}
+
+$(document).ready(initiateRoleSlider);
---
+++ themes/ziro/assets/js/homeBackground.js
@@ -0,0 +1,23 @@
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+$("#backgroundRenderer").append( renderer.domElement );
+
+
+var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+var material = new THREE.MeshBasicMaterial( { color: 0xffe100 } );
+var cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+camera.position.y = 1;
+camera.position.x = 1;
+
+function animate() {
+	requestAnimationFrame( animate );
+	renderer.render( scene, camera );
+}
+
+animate();
