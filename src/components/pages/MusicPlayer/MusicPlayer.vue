b755369067c2f43d1d5acdcf41bf8e51e98bc4e9
Robinson Lam <robinsonlam94@gmail.com>
2023-03-18 00:47:32 +1100

    add search input. TODO: connect to api to search songs

--- src/components/pages/MusicPlayer/MusicPlayer.vue
+++ src/components/pages/MusicPlayer/MusicPlayer.vue
@@ -1,13 +1,25 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 
-defineProps<{ msg: string }>()
+let songSearchInput = ref(undefined);
+let songResultList = ref(undefined);
 
-const count = ref(0)
+const searchSongName = (e) => {
+  e.preventDefault();
+  console.log("I just called to say I:", songSearchInput.value);
+
+  let searchValue = songSearchInput.value;
+  console.log("TODO: Send request to server to search Spotify API for tracks and return them")
+};
+
 </script>
 
 <template>
-  <p>Music Player</p>
+  <h3>Welcome to the music player, search a song and see what happens</h3>
+  <form @submit="e => searchSongName(e)">
+    <input type="text" placeholder="Enter a song name..." v-model="songSearchInput"/>
+    <button type="submit">Search Songs</button>
+  </form>
 </template>
 
 <style scoped>
